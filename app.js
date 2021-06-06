54abfee8c295d7484a10ebe002befe8371eb61dc
irfanmusthafaa <irfanmustafa2307@gmail.com>
2021-06-06 17:49:01 +0700

    deploy heroku

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const flash = require('connect-flash');
 
 // import mongoose
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/db_staycation', {
+mongoose.connect('mongodb+srv://irfanmustafa2307:irfan2307@cluster0.b6wun.mongodb.net/db_staycation?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
